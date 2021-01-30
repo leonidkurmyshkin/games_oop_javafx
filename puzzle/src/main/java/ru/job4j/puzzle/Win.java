f48ae4462cd3818a5403e8b624c2b3c34ae1c403
leonidkurmyshkin <leonid2073@gmail.com>
2021-01-30 22:31:01 +0300

    2. Сокобан. Тестовое задание. [#446833]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,36 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1
+                    && (checkRow(board, i) || checkCol(board, i))) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkRow (int[][] board, int row) {
         boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean checkCol (int [][] board, int col) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] != 1) {
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
     }
 }
